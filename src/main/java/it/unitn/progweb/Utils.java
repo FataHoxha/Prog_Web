a635c74f9f0674cd2555c0a7f48021e822ebf6c3
Davide Pizzolotto <itspesa@gmail.com>
2015-06-08 16:06:20 +0200

    Scheletro base header/footer

--- src/main/java/it/unitn/progweb/Utils.java
+++ src/main/java/it/unitn/progweb/Utils.java
@@ -8,7 +8,7 @@ public class Utils {
     final private static Logger logger = Logger.getLogger(Utils.class.getName());
     final private static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
-    public static final String bytesToHex(byte[] bytes) {
+    private static final String bytesToHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
         for ( int j = 0; j < bytes.length; j++ ) {
             int v = bytes[j] & 0xFF;
