d44261a1ddc4960e087a53257a32c33909a07d63
Fabiano <fz@fabianozenatti.it>
2015-06-19 14:33:33 +0200

    abcde

--- src/main/java/it/unitn/progweb/model/Seat.java
+++ src/main/java/it/unitn/progweb/model/Seat.java
@@ -1,7 +1,55 @@
 package it.unitn.progweb.model;
 
-/**
- * Created by fabianozenatti on 19/06/15.
- */
 public class Seat {
+
+    private Integer id;
+    private Integer theater_id;
+    private Integer row;
+    private Integer column;
+    private boolean exist;
+    private int available;
+
+    public Seat(){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getTheater_id() {
+        return theater_id;
+    }
+
+    public void setTheater_id(Integer theater_id) {
+        this.theater_id = theater_id;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+
+    public boolean getExist() {
+        return exist;
+    }
+
+    public void setExist(boolean exist) {
+        this.exist = exist;
+    }
+
+
 }
