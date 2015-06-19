e5dcec42462c92fb5f7d4b68444ab8250adfedd7
Fabiano <fz@fabianozenatti.it>
2015-06-19 12:07:56 +0200

    Seleziona tipo biglietto nel jsp

--- src/main/java/it/unitn/progweb/controller/AdminAreaServlet.java
+++ src/main/java/it/unitn/progweb/controller/AdminAreaServlet.java
@@ -1,5 +1,10 @@
 package it.unitn.progweb.controller;
 
+import it.unitn.progweb.model.Price;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,10 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-/**
- * Created by fabianozenatti on 17/06/15.
- */
+import java.util.List;
 
 @WebServlet(name = "AdminAreaServlet", urlPatterns = {"/adminarea"})
 public class AdminAreaServlet extends HttpServlet {
@@ -19,7 +21,10 @@ public class AdminAreaServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
         RequestDispatcher rd = request.getRequestDispatcher("templates/admin.jsp");
         rd.forward(request, response);
     }
+
 }
--- src/main/java/it/unitn/progweb/controller/ReservationServlet.java
+++ src/main/java/it/unitn/progweb/controller/ReservationServlet.java
@@ -1,5 +1,10 @@
 package it.unitn.progweb.controller;
 
+import it.unitn.progweb.model.Price;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by davide on 15/06/18.
@@ -18,6 +24,20 @@ public class ReservationServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Sql2o database = (Sql2o) getServletContext().getAttribute("database");
+        String currentMovies = "select * from \"price\"";
+        List<Price> prices;
+        try(Connection conn = database.open()){
+            prices = conn.createQuery(currentMovies).executeAndFetch(Price.class);
+        } catch (Sql2oException exc) {
+            throw exc;
+        }
+
+        request.setAttribute("prices", prices);
+
+
+
         RequestDispatcher rd = request.getRequestDispatcher("templates/reservation.jsp");
         rd.forward(request, response);
     }
