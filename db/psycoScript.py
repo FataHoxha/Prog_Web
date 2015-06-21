f158e3b8a2b3aac1ed229a2b4c612cf990d8b536
gas <ilgaspa93@gmail.com>
2015-06-21 22:30:32 +0200

    commenti e fix seats_id

--- db/psycoScript.py
+++ db/psycoScript.py
@@ -26,6 +26,7 @@ conn = psycopg2.connect("dbname=movies user=movies")
 #cursor to perform database operations
 cur = conn.cursor()
 
+#query
 strtmp = ""
 
 ##### SHOW #####
@@ -33,33 +34,41 @@ for movie in range(1,31):
 	for i in range(string.atoi(numProg)):
 		strtmp = "insert into \"show\" (id, movie_id, theater_id, date_time) VALUES('"+str(id)+"', '"+str(movie)+"', '"+str(sala)+"', %s);"
 		cur.execute(strtmp, (date.strftime("%Y-%m-%d %H:%M:%S"), ))
-		date = date + datetime.timedelta(minutes = 5)
+		#adding interval
+		date = date + datetime.timedelta(minutes = int(intervallo))
+		#incrementing id
 		id +=1
-
+	#theater control
 	if((sala+1)%5 == 0):
 		sala = 1
 		date = date + datetime.timedelta(minutes = 1)
 		date_orig = date
 	else:
+		#refreshing date, different theaters can have a show at the same moment
 		date = date_orig
 		sala += 1
 conn.commit()
 
 
 #### RESERVATIONS ####
+# numProg shows for each film
 MAXSHOW = 30* string.atoi(numProg)
 
+#choosing random user
 user = randint(1,15)
-
+#db parameters
 price = 1
-
 show = 1
-giro = 1
 sala = 1
 posto = randint(1,4)
+#number of reservations (10 in this case)
+giro = 1
+idposto = posto
 
 while(1):
+	#choosing seat
 	posto = posto + randint(1,10)
+	#max of 50 seats
 	if(posto > 50 ):
 		giro = 1
 		show += 1
@@ -70,11 +79,18 @@ while(1):
 		if(show == MAXSHOW-1):
 			break
 		posto = randint(1,4)
-		
-	strtmp = "insert into \"reservation\" (user_id, show_id, price_id, seat_id) VALUES ('"+str(user)+"','"+str(show)+"','"+str(price)+"','"+str(posto)+"');\n"
+	idposto = posto
+	if(sala == 2):
+		idposto = idposto + 50
+	if(sala == 3):
+		idposto = idposto + 106
+	if(sala == 4):
+		idposto = idposto + 159
+	strtmp = "insert into \"reservation\" (user_id, show_id, price_id, seat_id) VALUES ('"+str(user)+"','"+str(show)+"','"+str(price)+"','"+str(idposto)+"');\n"
 	cur.execute(strtmp)
 	user = randint(1,15)
 	giro+=1
+	#10 reservations? -> reset
 	if(giro == 10):
 		giro == 1
 		show += 1
